e26367c1354a243d4e8a9e28d418ac08a4273af1
Kevin <mr.kev@me.com>
2017-05-01 17:03:25 -0400

    LIT BOIS

--- js/visualizer.js
+++ js/visualizer.js
@@ -35,8 +35,10 @@ class SteveMarschnersDreamVisualizer {
    */
   init() {
 
+    // functions [0,1] for animating to different things
     const iKick = () => this.bandan.getFrequencyValue(22)
     const iHigh = () => this.bandan.getFrequencyValue(18000)
+    const iTime = () => this.animation_i
 
     return Promise.resolve(new ResourceLoader(this))
       // audio buffer needs to be loaded to 'audio', also load meshes and textures
@@ -47,9 +49,10 @@ class SteveMarschnersDreamVisualizer {
       // things have been loaded 
       .then(_ => {
 
-        const sphere = new AObject(this.gl, this.sphere_obj, this.earthImage)
-        sphere.animation = new Animation(sphere, iKick);
-        sphere.animation.addSequence([
+        /** One sphere */
+        const kick_sphere = new AObject(this.gl, this.sphere_obj, this.earthImage)
+        kick_sphere.animation = new Animation(kick_sphere, iHigh);
+        kick_sphere.animation.addSequence([
             anim.translate(1,0,0),
             anim.compose([
                 anim.translate(-1,0,0),
@@ -57,9 +60,46 @@ class SteveMarschnersDreamVisualizer {
                 anim.translate(1, 0, 1)
             ])
         ])
+        this.objects.push(kick_sphere)
 
-        this.objects.push(sphere)
+        /** Another sphere */
+        const high_sphere = new AObject(this.gl, this.sphere_obj, this.earthImage)
+        high_sphere.animation = new Animation(high_sphere, iKick);
+        high_sphere.animation.addSequence([
+            anim.compose([
+                anim.translate(-1,0,0),
+                anim.scale(0.2,1),
+                anim.translate(1, 0, 1)
+            ])
+        ])
+        this.objects.push(high_sphere)
 
+
+        /** one more */
+        const kick_sphere2 = new AObject(this.gl, this.sphere_obj, this.earthImage)
+        kick_sphere2.animation = new Animation(kick_sphere2, iKick);
+        kick_sphere2.animation.addSequence([
+          anim.translate(1,0,0),
+          anim.translate(0,1,0),
+          anim.translate(-2,0,0),
+          anim.translate(0,-2,0),
+          anim.translate(2,0,0),
+          anim.translate(0,2,0)
+        ])
+        this.objects.push(kick_sphere2)
+
+
+        // const time_sphere = new AObject(this.gl, this.sphere_obj, this.earthImage)
+        // time_sphere.animation = new Animation(time_sphere, iTime);
+        // time_sphere.animation.addSequence([
+        //   anim.translate(1,0,0),
+        //   anim.translate(0,1,0),
+        //   anim.translate(-2,0,0),
+        //   anim.translate(0,-2,0),
+        //   anim.translate(2,0,0),
+        //   anim.translate(0,2,0)
+        // ])
+        // this.objects.push(time_sphere)
 
       })
       .then(_ => initVisualizer(this))
