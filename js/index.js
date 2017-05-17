06098cf09fdcbd6e1b954bbcea7490402ee20cb5
Kevin <mr.kev@me.com>
2017-05-17 18:46:06 -0400

    Audio selection

--- js/index.js
+++ js/index.js
@@ -31,18 +31,43 @@ if (!context.createDelay)
 if (!context.createScriptProcessor)
   context.createScriptProcessor = context.createJavaScriptNode;
 
-const playbutton = document.getElementById('playbutton')
-
 /** Create and start the visualizer */
 const visualizer = new SteveMarschnersDreamVisualizer($("#webglCanvas")[0])
 
 visualizer
 .init()
-.then((viz) => {
+.then(() => loadAudio(context, 'data/songs/vollekraftvoraus.mp3'))
+.then(audio => {
+  visualizer.setAudio(audio)
+})
+.then(() => {
   // playbutton.removeAttribute('disabled')
   // playbutton.innerHTML = 'Play/pause'
   // playbutton.addEventListener('click', () => { visualizer.togglePlayback() });
   document.addEventListener('keypress', (event) => {if (event.key == 'p'){ visualizer.togglePlayback() } else if (event.key == 'u'){ visualizer.explode()}})
-
 })
 .catch(console.error)
+
+const sel_builtin_audio = path => loadAudio(context, path).then(audio => {
+  visualizer.setAudio(audio)
+})
+
+$("#songChoice").change(function (){
+  var selectedsong = $("#selectedsong");
+  var songUpload = $("#songChoice")[0];
+
+  if ('files' in songUpload){
+    if (songUpload.files.length == 0){
+      console.log('no song selected');
+    } else {
+      var song = songUpload.files[0];
+      if ('name' in song){
+        selectedsong.text(song.name);
+        $("#customsong").prop("checked", true);
+        console.log('RIP')
+        // var loader = new ResourceLoader(this);
+        // loader.loadAudio(name, name, context);
+      }
+    }
+  }
+});
